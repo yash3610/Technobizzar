0c2e2af17929bc30569f29770f4faa33a1d62d7a
Yash Hule <huleyash3610@gmail.com>
2026-01-29 19:10:09 +0530

    a

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'https://techno-api.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
